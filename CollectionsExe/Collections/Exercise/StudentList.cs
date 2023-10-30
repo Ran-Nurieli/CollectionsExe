da99ecbe93d9c41c9f602e53eb1a51511e61621c
User <User@LAB3-3>
2023-10-30 13:04:19 +0200

    siyamti

--- CollectionsExe/Collections/Exercise/StudentList.cs
+++ CollectionsExe/Collections/Exercise/StudentList.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using CollectionsExe.Collections.Exercise.Models;
 
 namespace CollectionsExe.Collections.Exercise
@@ -14,36 +15,44 @@ namespace CollectionsExe.Collections.Exercise
 
         //Targil 1 - write the Property of StudentList called Students
         #region Properties
-       
+
 
         #endregion
+        public List<Student> Students { get; set; }
 
         //Targil 2 - Write a constructor which initiates the students and calls for Student.FillList()
         #region Constructor
         public StudentList()
         {
-
+            Students = Student.FillList();
         }
         #endregion
-
-
-
 
         #region Find Student By Name
         //Targil 3- write a method which retrieves a Student By name. 
 
         public Student FindStudentByName(string name)
         {
+            foreach(Student student in Students)
+            {
+                if(student.Name == name) return student;
+            }
             return null;
         }
         #endregion
+        
 
         #region retrieving a list
         //Targil 4-  write a method which retrieves a list of Students By Kita
-        public List<Student> GetStudentsByKits(string kita)
+        public List<Student> GetStudentsByKits(int kita)
         {
-            return null;
-        }
+            List<Student> students2 = new List<Student>();
+            foreach (Student student in Students)
+            {
+                if (student.Kita == kita) { students2.Add(student); }
+            }
+            return students2;
+        } 
         #endregion
 
 
